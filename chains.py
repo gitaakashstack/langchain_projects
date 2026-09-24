e6af9e6924bd02e2477706443a4c46989b751f0f
Aakash Pandey <apandey@cashcontrol.de>
2026-09-24 16:04:02 +0200

    reflexion agent done

--- chains.py
+++ chains.py
@@ -1,6 +1,7 @@
 import datetime
 
 from dotenv import load_dotenv
+from langchain_openai import ChatOpenAI
 
 load_dotenv()
 from langchain_core.messages import HumanMessage
@@ -8,7 +9,7 @@ from langchain_core.output_parsers import JsonOutputToolsParser, PydanticToolsPa
 from langchain_core.prompts import ChatPromptTemplate, MessagesPlaceholder
 from langchain_google_genai import ChatGoogleGenerativeAI
 
-from schemas import AnswerQuestion
+from schemas import AnswerQuestion, ReviseAnswer
 
 actor_prompt_template = ChatPromptTemplate.from_messages(
     [
@@ -33,17 +34,29 @@ actor_prompt_template = ChatPromptTemplate.from_messages(
 )
 
 first_responder_prompt_template = actor_prompt_template.partial(
-    first_instruction="Provide a detailed ~205 word answer"
+    first_instruction="Provide a ~50 word answer"
 )
 
 
-llm = ChatGoogleGenerativeAI(model="gemini-3.6-flash", temperature=0)
+llm = ChatOpenAI(model="gpt-6-luna")
 parser = JsonOutputToolsParser(return_id=True)
 parser_pydantic = PydanticToolsParser(tools=[AnswerQuestion])
 
 first_responder = first_responder_prompt_template | llm.bind_tools(
     tools=[AnswerQuestion], tool_choice="AnswerQuestion"
 )
+
+revise_instructions = """Revise your previous answer using the new information.
+    - You should use the previous critique to add important information to your answer.
+        - You MUST include numerical citations in your revised answer to ensure it can be verified.
+        - Add a "References" section to the bottom of your answer (which does not count towards the word limit). In form of:
+            - [1] https://example.com
+            - [2] https://example.com
+    - You should use the previous critique to remove superfluous information from your answer and make SURE it is not more than 50 words.
+"""
+revisor = actor_prompt_template.partial(
+    first_instruction=revise_instructions
+) | llm.bind_tools(tools=[ReviseAnswer], tool_choice="ReviseAnswer")
 
 if __name__ == "__main__":
     human_message = HumanMessage(
